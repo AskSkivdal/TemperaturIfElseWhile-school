a972671d7ea7990f9c73c07246bbf857e5e5bd11
asciiboi <56585404+asciiboi@users.noreply.github.com>
2023-08-31 22:34:07 +0200

    Initial commit

---
+++ TemperaturIfElseWhile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturIfElseWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input a number: ");
+                string value = Console.ReadLine();
+                int number = 0;
+                if (int.TryParse(value, out number)) {
+                    if (number >= 0)
+                    {
+                        Console.WriteLine(number + " is positive");
+                    } else
+                    {
+                        Console.WriteLine(number + " is negative");
+                    }
+                } else
+                {
+                    Console.WriteLine("invalid input");
+                }
+            }
+
+
+        }
+    }
+}
